d6ff47b52b180fc74271f91678fb5b0346be170f
Daniel Okenka <danokenka@me.com>
2020-08-27 12:18:01 -0400

    FINISHED MODAL FOR NEW TASK NOT LINKED YET

---
+++ myHomeschoolLog/Views/TaskView.swift
@@ -0,0 +1,56 @@
+//
+//  TaskView.swift
+//  myHomeschoolLog
+//
+//  Created by Dan O on 8/27/20.
+//  Copyright © 2020 People Like Us LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    // define property and instance
+    var taskStore: TaskStore
+    @State var modalIsPresented = false
+    
+    var body: some View {
+
+        NavigationView {
+            // New way
+            List(taskStore.tasks, id: \.id) { task in
+                Text(task.name)
+            }
+        .navigationBarTitle("Tasks")
+        .navigationBarItems(
+            trailing:
+                Button(
+                    action: { self.modalIsPresented = true
+                    
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+        .sheet(isPresented: $modalIsPresented) {
+            NewTaskView()
+        }
+
+        
+
+        
+                //old way
+                
+        //        List(taskStore.tasks.indices) { index in
+        //            Text(self.taskStore.tasks[index].name)
+        //        }
+    }
+    
+    
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView(taskStore: TaskStore())
+    }
+}
+
