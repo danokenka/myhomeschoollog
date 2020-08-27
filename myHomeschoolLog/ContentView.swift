48c802ca80f9dbf1adf245f79eb2c1c72a1b4a15
Daniel Okenka <danokenka@me.com>
2020-08-26 20:07:56 -0400

    Initial Commit

---
+++ myHomeschoolLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myHomeschoolLog
+//
+//  Created by Dan O on 8/26/20.
+//  Copyright © 2020 People Like Us LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
